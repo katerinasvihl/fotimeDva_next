52937d5e343110b36ebbc0199f0c965486d7edbb
marek <marek.chasak@seznam.cy>
2025-12-16 17:49:57 +0100

    prettier fix

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
@@ -12,9 +12,9 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placeholder.co',
-      }
-    ]
-  }
-};
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
